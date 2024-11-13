cfb3b55472ec4019ac09da5c01319655d94cc63d
Lalith <lalithmulpuri@gmail.com>
2024-11-13 04:24:51 -0500

    Added comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 export default function Home() {
+  // Love you
   return (
     <div>
       <h1> Enter your name:</h1>
 
       <form>
-        <input type="text" placeholder="Type your name here..." />
+        <input type="text" placeholder="Type your name here..." /> 
         <button type="submit"> Predict Data</button>
       </form>
     </div>
