37574bd92446716ee87f37b703479942b5cf42aa
Lalith <lalithmulpuri@gmail.com>
2024-11-13 04:09:32 -0500

    Added files

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1> Enter your name:</h1>
+
+      <form>
+        <input type="text" placeholder="Type your name here..." />
+        <button type="submit"> Predict Data</button>
+      </form>
+    </div>
+  );
+}
